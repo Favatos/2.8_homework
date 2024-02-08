a976c1ea240fd7e2dd3ab70c9ccd64ef15048dae
werty werty <werty36401@gmail.com>
2024-02-08 14:07:28 +0300

    Добавьте файлы проекта.

---
+++ 2.8_дз/2.8_дз.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+struct Player
+{
+	weak_ptr<Player> companion;
+	~Player() { cout << "~Player\n"; }
+};
+
+int main()
+{
+	std::shared_ptr<Player> jasmine = make_shared<Player>();
+	std::shared_ptr<Player> albert = make_shared<Player>();
+
+	jasmine->companion = albert; 
+	albert->companion = jasmine; 
+
+	return 0;
+}
